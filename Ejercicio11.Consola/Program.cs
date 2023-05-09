49110c5b4b678db110f34cb5250a3d4dfe3f4895
BiancaLopresto <bianlopre19@gmail.com>
2023-05-09 19:35:39 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio11.Consola/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Ejercicio11.Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Ladomenor, LadoMayor;
+            int Perimetro, Superficie;
+
+            Console.Write("Ingrese el Lado menor: ");
+            Ladomenor=int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el Lado mayor: ");
+            LadoMayor=int.Parse(Console.ReadLine());
+
+            Superficie = LadoMayor * Ladomenor;
+            Perimetro = 2 * LadoMayor + 2 * Ladomenor;
+
+            Console.WriteLine($"El perimetro es {Perimetro} y la superficie es {Superficie}");
+        }
+    }
+}
